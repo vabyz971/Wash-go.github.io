ccd22500b3554da5e9734d1745c898e5e67fbb1c
vabyz971 <vabyz971@gmail.com>
2026-02-02 09:56:05 -0500

    fix dependance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app:{
-    baseURL: '/Wash-go/',
+    baseURL: '/',
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
